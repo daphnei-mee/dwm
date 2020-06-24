1bce5cbb276e2b0a83f573a9d59a12af7e863553
Jacob <jacobmee@tutanota.com>
2020-06-24 15:55:40 -0400

    fixed steam window moving around & adjusted border pixel to 2

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 50;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 50;       /* vert inner gap between windows */
@@ -14,6 +14,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+/*static const char *fonts[]          = { "monospace:size=10", "FontAwesome:size=80:antialias=true:autohint=true"};*/
 static const char *fonts[]          = { "monospace:size=10", "JoyPixels:size=10:antialias=true:autohint=true"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
