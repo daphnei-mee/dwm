2990b7b454ad0a28964f350bcb08a78e8c8d151a
Jacob <jacobmee@tutanota.com>
2020-06-24 01:51:41 -0400

    added future option for fontawesome

--- config.def.h
+++ config.def.h
@@ -14,6 +14,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+/*static const char *fonts[]          = { "monospace:size=10", "FontAwesome:size=80:antialias=true:autohint=true"};*/
 static const char *fonts[]          = { "monospace:size=10", "JoyPixels:size=10:antialias=true:autohint=true"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
