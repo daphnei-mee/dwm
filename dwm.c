90b1453f171daa631d0393bb3dfacac355526646
Jacob <jacobmee@tutanota.com>
2020-07-07 00:44:22 -0400

    made new floating windows appear in the centre rather than top-left

--- dwm.c
+++ dwm.c
@@ -1268,6 +1268,11 @@ manage(Window w, XWindowAttributes *wa)
 	c->bw = borderpx;
 
 	wc.border_width = c->bw;
+
+	/* CENTER FLOATING WINDOWS */
+	if (c->x == selmon->wx) c->x += (c->mon->ww - WIDTH(c)) / 2 - c->bw;
+	if (c->y == selmon->wy) c->y += (c->mon->wh - HEIGHT(c)) / 2 - c->bw;
+
 	XConfigureWindow(dpy, w, CWBorderWidth, &wc);
 	XSetWindowBorder(dpy, w, scheme[SchemeNorm][ColBorder].pixel);
 	configure(c); /* propagates border_width, if size doesn't change */
